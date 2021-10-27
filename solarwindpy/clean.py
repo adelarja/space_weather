6d84b7f560d04ea1b36dca3d66604f03e63153d4
piedra88 <ricardo.pafundi@gmail.com>
2021-10-27 11:33:02 -0300

    clean1

--- solarwindpy/clean.py
+++ solarwindpy/clean.py
@@ -8,7 +8,7 @@ Created on Wed Oct 27 10:03:31 2021
 
 import numpy as np
 def clean(bx,by,bz, error_flag=1e+20):
-    bx[abs(bx) < error_flag] = np.NaN
-    by[abs(by) < error_flag] = np.NaN
-    bz[abs(bz) < error_flag] = np.NaN
+    bx[abs(bx) > error_flag] = np.NaN
+    by[abs(by) > error_flag] = np.NaN
+    bz[abs(bz) > error_flag] = np.NaN
     return bx, by, bz
