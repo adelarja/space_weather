67779f146c4194dded5119a29e98d45297c0a986
VioletaBazzano <89461552+VioletaBazzano@users.noreply.github.com>
2022-01-16 15:26:15 -0300

    Agrego Licencia

--- solarwindpy/__init__.py
+++ solarwindpy/__init__.py
@@ -1,1 +1,10 @@
+#This file is part of the
+#	Solarwindpy Project(https://github.com/adelarja/space_weather).
+
+#Copyright (c) 2021, Adriana Gulisano, Adel Arja, Ricardo Pafundigit, Violeta Bazzano.
+#All rights reserved.
+
+#License: BSD 3-Clause License
+#	Full Text: https://github.com/adelarja/space_weather/blob/main/LICENSE
+
 __version__ = "0.0.1"
