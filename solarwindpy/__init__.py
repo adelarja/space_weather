58f6d0e48ca9b9c56ccbad78e7b8d93b9bbf6c6a
adelarja <adelarja@gmail.com>
2022-02-11 16:28:04 -0300

    Added tests for docstyle. Added docstring

--- solarwindpy/__init__.py
+++ solarwindpy/__init__.py
@@ -1,11 +1,16 @@
 # This file is part of the
-# 	Solarwindpy Project(https://github.com/adelarja/space_weather).
+# Solarwindpy Project(https://github.com/adelarja/space_weather).
 
 # Copyright (c) 2021, Adriana Gulisano, Adel Arja, Ricardo Pafundigit,
 # Violeta Bazzano.
 # All rights reserved.
 
 # License: BSD 3-Clause License
-# 	Full Text: https://github.com/adelarja/space_weather/blob/main/LICENSE
+# Full Text: https://github.com/adelarja/space_weather/blob/main/LICENSE
+"""Solarwindpy.
+
+A package to process Magnetic Fields Data and analyze
+magnetic storm phenomena.
+"""
 
 __version__ = "0.0.1"
