a40e7200bcb492cd82ff089374ca0c2262ec63fd
VioletaBazzano <89461552+VioletaBazzano@users.noreply.github.com>
2022-02-15 10:51:08 -0300

    Agrego Licencia

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,15 @@
+# This file is part of the
+# Solarwindpy Project(https://github.com/adelarja/space_weather).
+
+# Copyright (c) 2021, Adriana Gulisano, Adel Arja, Ricardo Pafundigit, Violeta Bazzano.
+# All rights reserved.
+
+# License: BSD 3-Clause License
+# Full Text: https://github.com/adelarja/space_weather/blob/main/LICENSE
+
+
 # Configuration file for the Sphinx documentation builder.
-#
+
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
