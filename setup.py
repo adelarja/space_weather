f77799d2a099ee0216e01c8b74a671bbc5906157
VioletaBazzano <89461552+VioletaBazzano@users.noreply.github.com>
2022-02-15 10:48:20 -0300

    Agrego Licencia

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+# This file is part of the
+# Solarwindpy Project(https://github.com/adelarja/space_weather).
+
+# Copyright (c) 2021, Adriana Gulisano, Adel Arja, Ricardo Pafundigit, Violeta Bazzano.
+# All rights reserved.
+
+# License: BSD 3-Clause License
+# Full Text: https://github.com/adelarja/space_weather/blob/main/LICENSE
+
+
 # =====================================================================
 # DOCS
 # =====================================================================
